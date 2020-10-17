b9472786fb053892fb9e5cddf5c9751cbcf89368
gabrielrbernardi <gabrielrbernardi@gmail.com>
2020-10-17 10:10:50 -0300

    Questao 1059 URI

---
+++ 2020-4/NEPS/157.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int qtd;
+    scanf("%d", &qtd);
+    int i = 0;
+    while(i < qtd){
+        printf("NepsAcademy eh Sucesso\n");
+        i++;
+    }
+}
---
+++ 2020-4/URI/1059.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    for(int i = 1; i <= 100; i++){
+        if(i % 2 == 0){
+            printf("%d\n", i);
+        }
+    }
+}
+
+// #include <stdio.h>
+
+// int main(){
+//     for(int i = 2; i <= 100; i+=2){
+//         printf("%d\n", i);
+//     }
+// }
