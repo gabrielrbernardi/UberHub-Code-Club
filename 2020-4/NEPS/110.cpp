a1c227a8048f9f3f20381137cd6c3fdb414f9b87
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2020-10-27 06:31:46 -0300

    Questao 110 Neps

--- 2020-4/NEPS/110.cpp
+++ 2020-4/NEPS/110.cpp
@@ -2,18 +2,26 @@
 
 using namespace std;
 
-long int vet[100000];
+long int vet[10000];
 int main(){
-    int valDigitado, qtd, status = -1, ma = 0;
+    int valDigitado, valDigitadoAnt = 0, qtd;
+    long int ma = 0;
     cin >> qtd;
-    for (int i = 0; i < qtd; i++){
+    cin >> valDigitado;
+    valDigitadoAnt = valDigitado;
+    vet[valDigitado-1]+=1;
+
+    for (int i = 1; i < qtd; i++){
         cin >> valDigitado;
-        vet[valDigitado-1]++;
+        if(valDigitado == valDigitadoAnt){
+            vet[valDigitado-1] += 1;
+        }else{
+            valDigitadoAnt = valDigitado;
+            vet[valDigitado-1] = 1;
+        }
     }
-    cout << 1 << endl;
-    for (int i = 0; i < 1000000; i++){
+    for (int i = 0; i < 10000; i++){
         if(vet[i] > ma){
-            cout << 2 << endl;
             ma = vet[i];
         }
     }
