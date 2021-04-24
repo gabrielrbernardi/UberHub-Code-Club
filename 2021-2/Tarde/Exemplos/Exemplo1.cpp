e1f6cca1540b6c99fd4259d4983c5f02ec29bc0e
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-04-24 14:52:53 -0300

    Exemplo 1 Tarde

---
+++ 2021-2/Tarde/Exemplos/Exemplo1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, soma;
+    cin >> a >> b;
+    soma = a + b;
+    printf("A soma de %d e %d eh: %d\n", a, b, soma);
+    cout << "A soma de " << a << " e " << b << " eh: " << soma << endl;
+}
+
+/// Faça um programa que some dois numeros e mostre o resultado
