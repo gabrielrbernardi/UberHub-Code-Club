42a40aff1afb6701d3840a101382ccae6a2e75d3
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-08-07 16:38:53 -0300

    Exercicios 1172 e 1177 URI Tarde

---
+++ 2021-4/Tarde/URI/1177.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int vet[1000];
+    int val;
+    cin >> val;
+    for(int i = 0, j = 0; i < 1000; i++, j++){
+        if(j < val){
+            vet[i] = j;
+        }else{
+            j = 0;
+            vet[i] = j;
+        }
+    }
+    // for(int i = 0; i < 1000; i++){
+    //     vet[i] = i % val;
+    // }
+    for(int i = 0; i < 1000; i++){
+        cout << "N[" << i << "] = " << vet[i] << endl;
+
+    }
+}
