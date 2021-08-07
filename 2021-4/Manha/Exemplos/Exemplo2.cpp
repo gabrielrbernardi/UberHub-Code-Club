be1a924d46cb488a7ff3894c74d5c1619e01fc90
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-08-07 10:36:20 -0300

    Exercicios 1001 e 1059 C++  Exemplos Manha

---
+++ 2021-4/Manha/Exemplos/Exemplo2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int val;
+    // scanf("%d", &val);
+    // printf("%d\n", 2*val);
+    cin >> val;
+    cout << 2*val << endl;
+    
+    return 0;
+}
