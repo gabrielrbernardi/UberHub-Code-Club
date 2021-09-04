022305ed6102325c233465a5c91adfb1798172cd
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-09-04 16:36:37 -0300

    Exercicios Neps Funcoes Tarde

---
+++ 2021-4/Tarde/NEPS/326_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int soma_vetor(int n, int v[]){
+    int soma = 0;
+	for(int i = 0; i < n; i++){
+        soma = soma + v[i];
+    }
+    return soma;
+}
+
+int main(){
+
+	int n, v[100100];
+	cin >> n;
+
+	for(int i=0;i<n;i++)
+		cin >> v[i];
+
+	cout << soma_vetor(n,v) << "\n";
+}
