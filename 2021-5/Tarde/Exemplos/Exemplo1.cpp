d1e5b14a7205df889af620fa7f9a8141b4f738e9
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-10-02 14:55:10 -0300

    Exemplos e exercicios URI Tarde

---
+++ 2021-5/Tarde/Exemplos/Exemplo1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float val = 10.0;
+
+    printf("O valor de val eh: %f\n", val);
+
+    cout << "O valor de val eh: " << val << endl;
+}
---
+++ 2021-5/Tarde/Exemplos/Exemplo2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float val;
+
+    scanf("%f", &val);
+
+    cin >> val;
+
+    printf("O valor de val eh: %.3f\n", val);
+
+    cout << fixed;
+    cout << "O valor de val eh: " << setprecision(3) << val << endl;
+}
