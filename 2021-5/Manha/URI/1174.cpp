ac506fe0f8282af9c3b98b4a5dcf9460ce3abc9f
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-10-02 11:53:55 -0300

    Exercicios URI

---
+++ 2021-5/Manha/URI/1174.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double vet[100];
+
+    for(int i = 0; i < 100; i++){
+        cin >> vet[i];
+    }
+
+    for(int i = 0; i < 100; i++){
+        if(vet[i] <= 10.0){
+            printf("A[%d] = %.1lf\n", i, vet[i]);
+        }
+    }
+}
