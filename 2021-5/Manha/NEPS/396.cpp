1d304c3304891953cb1c55380774b2b97a6c871b
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-10-09 15:53:26 -0300

    Exercicio NEPS Vector

---
+++ 2021-5/Manha/NEPS/396.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> vet, indices;
+    int aux, valorBusca;
+
+    for(int i = 0; i < 10; i++){
+        cin >> aux;
+        vet.push_back(aux);
+    }
+    cin >> valorBusca;
+
+    for(int i = 0; i < vet.size(); i++){
+        if(vet.at(i) == valorBusca){
+            indices.push_back(i);
+        }
+    }
+    if(indices.size() == 0){ ///nao encontrou o valor no vetor
+        cout << "Mia x" << endl;
+    }else{
+        cout << indices.size() << endl;
+        for(int i = 0; i < indices.size() - 1; i++){
+            cout << indices.at(i) << " ";
+        }
+        cout << indices.at(indices.size() - 1) << endl;
+    }
+}
