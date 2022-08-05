c98ef0f7ce19ad602d4d35e6a6845207e60010db
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2022-08-05 16:56:58 -0300

    Update files

---
+++ 2022-3/Implementações/Iniciante 1/1564.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int qtd;
+
+    while(cin >> qtd){
+        if(qtd > 0){
+            cout << "vai ter duas!" << endl;
+        }else{
+            cout << "vai ter copa!" << endl;
+        }
+    }
+
+}
+
