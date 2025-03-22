fdf3ea8211480917b99c884af31e357399196892
gabrielrbernardi <gabrielrbernardi@gmail.com>
2025-03-22 00:16:38 -0300

    Init 2025-1

---
+++ 2024-3_Nacional/Exemplos/Exemplo8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    int x = 1000;
+
+    if (((((((((x == 1000)))))))){
+        cout << "x is equal to 1000" << endl;
+    }else{
+        cout << "x is not equal to 1000" << endl;
+    }
+}
+
---
+++ 2025-1/Manha/Beecrowd/1172.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    
+}
